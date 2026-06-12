05e10f31f8da90d087c6ae86f23a57f847a7bebe
Nafiz <nafizmahmud52@gmail.com>
2026-06-12 11:51:08 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+// dummy data
+
+const dataElements = [
+  {
+    title: "Post title 1",
+    description: "Post description 1 ",
+  },
+  {
+    title: "Post title 2",
+    description: "Post description 2",
+  },
+  {
+    title: "Post title 3",
+    description: "Post description 3",
+  },
+  {
+    title: "Post title 4",
+    description: "Post description 4",
+  },
+  {
+    title: "Post title 5",
+    description: "Post description 5",
+  },
+  {
+    title: "Post title 6",
+    description: "Post description 6",
+  },
+  {
+    title: "Post title 7",
+    description: "Post description 7",
+  },
+  {
+    title: "Post title 8",
+    description: "Post description 8",
+  },
+];
+
+// <div class="post">
+// <h4 class="post-title">Post title 1</h4>
+// <p class="post-description">post description 1</p>
+// </div>
+
+const posts = document.querySelector(".posts");
+
+const dynamicCards = () => {
+  dataElements.map((post) => {
+    const postElement = document.createElement("div");
+    postElement.classList.add("post");
+
+    postElement.innerHTML = `
+  <h4 class="post-title">${post.title}</h4>
+ <p class="post-description">${post.description}</p>
+  `;
+    posts.appendChild(postElement);
+  });
+};
+
+dynamicCards();
